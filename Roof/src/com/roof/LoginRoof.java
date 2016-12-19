9c0cfb311bc37b0712f96f4757935d52c0f5ef94
CrazyGod <guppikan83@yahoo.com>
2016-12-19 20:23:40 +0530

    New changes for roof

--- Roof/src/com/roof/LoginRoof.java
+++ Roof/src/com/roof/LoginRoof.java
@@ -6,11 +6,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class LoginRoof {
       static WebDriver driver;
+     static  String url = "https://staging.roofstock.com";
 	public static void main(String[] args) {
 		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver-v0.11.1-win64\\geckodriver.exe");
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		driver = new FirefoxDriver(capabilities);
-		driver.get("https://staging.roofstock.com");
+		driver.get(url);
 	}
 
 }
